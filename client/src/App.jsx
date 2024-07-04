19702867babedde6640f1b4a864ab5d7ea4a24c7
nebanerj <neha.banerjee@oracle.com>
2024-07-04 17:14:50 +0530

    Added routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,19 @@
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 export default function App() {
   return (
-    <h1 className='text-red-400'>{"Hello world"}</h1>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/signIn" element={<SignIn />} />
+        <Route path="/signUp" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
