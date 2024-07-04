d3e7a91300e0c08b6bef6ac8f4ef43033c181f52
nebanerj <neha.banerjee@oracle.com>
2024-07-04 18:01:19 +0530

    Changes to add header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,14 +4,16 @@ import About from "./pages/About";
 import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import Header from "./components/Header";
 export default function App() {
   return (
     <BrowserRouter>
+    <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/about" element={<About />} />
-        <Route path="/signIn" element={<SignIn />} />
+        <Route path="/sign-in" element={<SignIn />} />
         <Route path="/signUp" element={<SignUp />} />
       </Routes>
     </BrowserRouter>
